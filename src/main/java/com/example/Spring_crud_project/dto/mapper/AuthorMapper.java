f54fc6b29bd744a4491201d96615340db5efbea6
David <david.m.jansson@gmail.com>
2026-05-13 10:07:48 +0200

    Removed Lomak

--- src/main/java/com/example/Spring_crud_project/dto/mapper/AuthorMapper.java
+++ src/main/java/com/example/Spring_crud_project/dto/mapper/AuthorMapper.java
@@ -4,7 +4,6 @@ package com.example.Spring_crud_project.dto.mapper;
 import com.example.Spring_crud_project.dto.classes.AuthorDto;
 import com.example.Spring_crud_project.entity.Author;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.NullValueCheckStrategy;
 
 @Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
--- src/main/java/com/example/Spring_crud_project/entity/Book.java
+++ src/main/java/com/example/Spring_crud_project/entity/Book.java
@@ -25,14 +25,6 @@ public class Book {
         return author;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
     public void setTitle(String title) {
         this.title = title;
     }
